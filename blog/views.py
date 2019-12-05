40ff089a118b918afc8a6ecc3066d05833730209
Mouctar <mouctardiallo07@gmail.com>
2019-12-05 17:23:52 +0100

    view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Question, Associer
+from django.db.models import Count
 
 # Create your views here.
 
@@ -9,5 +10,7 @@ def post_list(request):
 def jouer(request):
     question = Question.objects.get(cleQuestion=1)
     associee = Associer.objects.filter(question = question)
+ #   q = Associer.objects.annotate(number_of_entries = Count('question'))
+    q = Associer.objects.filter(question = question).count()
 
-    return render(request, 'blog/liens.html', {'question': question, 'associee': associee})
+    return render(request, 'blog/liens.html', {'question': question, 'associee': associee,'q': q})
