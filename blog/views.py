d9577ccbfebe51ae164ae9897c3294d9bdf9eb89
aarid <aaridouane@gmail.com>
2019-12-05 13:24:22 +0100

    pas d'avancée au niveau des tags et réponses

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Question, Associer
+from django.db.models import Count
 
 # Create your views here.
 
@@ -7,6 +8,14 @@ def post_list(request):
     return render(request, 'blog/acceuil.html', {})
 
 def jouer(request):
+    question = Question.objects.get(cleQuestion=1)
+    associee = Associer.objects.filter(question = question)
+    #tag = 
+
+    return render(request, 'blog/liens.html', {'question': question, 'associee': associee})
+
+
+def after_reponse(request):
     question = Question.objects.get(cleQuestion=1)
     associee = Associer.objects.filter(question = question)
 
