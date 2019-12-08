06105d942dfee48844f3016c1449206f4c0774be
aarid <aaridouane@gmail.com>
2019-12-08 04:30:43 +0100

    mise en place du powerpoint pour la démo

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('jouer/', views.jouer, name='play_button'),
-    path('jouer/after_reponse/', views.jouer, name='play_button'),
 ]
