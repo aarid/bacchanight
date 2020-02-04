0164edd51a78ab6f68952ed49488e8166d2a387b
obah001 <ousmane.bah@etu.u-bordeaux.fr>
2020-02-04 20:20:05 +0100

    contacter

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,10 @@ class AnswerForm(forms.Form):
     tag = forms.CharField(max_length=200, required=False)
     no_tag = forms.CharField(max_length=200, required=False)
     question_asked = forms.IntegerField()
+
+class ContacterNous(forms.Form):
+    nomPrenom = forms.CharField(max_length=100)
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender  = forms.CharField()
+    cc_myself = forms.BooleanField(required=False)
